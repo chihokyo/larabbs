c2443a4553c2e09f1406be7258816a9f9577f93b
vagrant <vagrant@homestead>
2018-11-03 06:30:41 +0000

    List topics created by user

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,5 +27,8 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
