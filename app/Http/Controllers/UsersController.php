160aed889aebdcd8784ea7478e70a3659b359a8c
vagrant <vagrant@homestead>
2018-11-03 05:01:13 +0000

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,6 +10,12 @@ use App\Handlers\ImageUploadHandler; //引入工具类utility class
 
 class UsersController extends Controller
 {
+    // 该方法接收两个参数，第一个为中间件的名称，第二个为要进行过滤的动作。
+    //Auth 中间件在过滤指定动作时，如该用户未通过身份验证（未登录用户），将会被重定向到登录页面
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
 
    /* Laravel 会自动解析定义在控制器方法（变量名匹配路由片段）中的 Eloquent 模型类型声明
     1). 路由声明时必须使用 Eloquent 模型的单数小写格式来作为 路由片段参数，User 对应 {user}：
@@ -21,12 +27,17 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        //authorize 方法接收两个参数，第一个为授权策略的名称，第二个为进行授权验证的数据。
+        $this->authorize('update', $user);
 
         return view('users.edit',compact('user'));
     }
 
     public function update(UserRequest $request,ImageUploadHandler $uploader,User $user)
     {
+        //authorize 方法接收两个参数，第一个为授权策略的名称，第二个为进行授权验证的数据。
+        $this->authorize('update', $user);
+        
         $data = $request->all();
 
         if ($request->avatar) {
